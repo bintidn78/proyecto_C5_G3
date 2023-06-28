90716fd8d35f7f8d4cae5f03cf98894cf652cb05
bintidin78 <bernardodinerstein@hotmail.com>
2023-06-28 09:05:47 -0300

    primera pagina del grupo 3

---
+++ board/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("¡¡¡¡¡¡Pagina WEB del Grupo 3!!!!!!")
+# Create your views here.
